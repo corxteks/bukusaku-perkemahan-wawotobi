66745c616bf1ad66b59d84edb66a0facb02508ce
corxteks <just.juki13@gmail.com>
2026-09-20 17:51:00 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,8 @@
-// Booklet mandiri: seluruh jadwal dan isi kegiatan berasal dari data lokal.
-window.BUKU_SAKU = {};
+// Koneksi Google Sheets opsional. Jika gagal, aplikasi tetap memakai data lokal.
+window.BUKU_SAKU = {
+  sheets: {
+    jadwal: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQsp5EGNW-EsghwFLUgFUp6AE8idXaPlzwtl5hSSJNvkTQI8qY9VLCPHw1X5_yKZg/pub?gid=1326021638&single=true&output=csv",
+    pengumuman: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQsp5EGNW-EsghwFLUgFUp6AE8idXaPlzwtl5hSSJNvkTQI8qY9VLCPHw1X5_yKZg/pub?gid=956249130&single=true&output=csv"
+  },
+  pollingMs: 300000
+};
