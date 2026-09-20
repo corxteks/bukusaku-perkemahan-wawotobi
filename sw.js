d06cb37850f0e62bd2c0f7a1b6bc9bcd5121dba0
corxteks <just.juki13@gmail.com>
2026-09-21 00:24:37 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "buku-saku-kwaran-v7";
+const CACHE_NAME = "buku-saku-kwaran-v8";
 const CORE_ASSETS = [
   "./",
   "./index.html",
@@ -13,6 +13,7 @@ const CORE_ASSETS = [
   "./maskot-pramuka-cutout.webp",
   "./qrcode-booklet-perkemahan.png"
   ,"./maskot-anoa-anim.webp"
+  ,"./maskot-anoa-chromakey.webm"
   ,"./narasi/sampul.mp3","./narasi/tema.mp3","./narasi/foto.mp3","./narasi/kepanitiaan.mp3","./narasi/bekal.mp3","./narasi/hari3.mp3","./narasi/hari0.mp3","./narasi/hari1.mp3","./narasi/hari2.mp3"
   ,"./foto/foto-1.jpg","./foto/foto-2.jpeg","./foto/foto-3.jpeg","./foto/foto-4.jpeg","./foto/foto-5.jpg","./foto/foto-6.jpeg","./foto/foto-7.jpeg","./foto/foto-8.jpeg","./foto/foto-9.jpeg","./foto/foto-10.jpeg","./foto/foto-11.jpeg","./foto/foto-12.jpeg","./foto/rustam-tabara.webp"
 ];
